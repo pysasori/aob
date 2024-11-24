2c3630a75f9a83b87bc104f5069b4ae58e42d2a9
Vladislav Krikunov <cacopu06071998@gmail.com>
2024-11-24 22:52:57 +0300

    add kill windows

--- bot/functions/kill_process_by_window_name.py
+++ bot/functions/kill_process_by_window_name.py
@@ -3,10 +3,9 @@ import psutil
 
 def kill_process_by_window_name(window_name):
     for proc in psutil.process_iter(['pid', 'name']):
-        print(proc.info['name'])
+
         if proc.info['name'] == window_name:
             print('убиваем window_name')
             proc.kill()
 
 
-kill_process_by_window_name('window_name')
